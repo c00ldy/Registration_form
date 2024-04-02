47f238e677591b91335a650ba892ab5fdbbfd4c5
c00ldy <mevadadhruval7529@gmail.com>
2024-04-02 11:53:59 +0530

    modified the projects

--- Registration Form/form.js
+++ Registration Form/form.js
@@ -4,27 +4,27 @@ errors.forEach((e) => {
     e.addEventListener('blur', (event) => {
 
         const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-console.log(event.target.name)
+        console.log(event.target.name)
 
         if (event.target.value == "") {
-           document.querySelector('.error').innerHTML = "enter valid details"
+            document.querySelector('.error').innerHTML = "enter valid details"
             console.log("done")
             return false;
         }
-        else if(event.target.value != ""){
+        else if (event.target.value != "") {
             console.log(event.target.type)
             event.target.classList.remove('isinvalid')
-            if(event.target.type == "email"){
+            if (event.target.type == "email") {
                 if (!emailRegex.test(event.target.value)) {
                     document.querySelector('.eerror').innerHTML = "please enter valid email"
                     return false;
                 }
-                else{
+                else {
                     document.querySelector('.eerror').innerHTML = ""
                 }
             }
         }
-        else{
+        else {
             event.target.classList.remove('isinvalid')
         }
         return true;
